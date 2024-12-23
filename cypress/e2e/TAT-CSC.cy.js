93488a6a8724fc8c7f505dca131db32ae5d669a8
Pedro Bondal <pedroyamada@gmail.com>
2024-12-23 15:53:36 -0300

    uploading files with cypress

--- cypress/e2e/TAT-CSC.cy.js
+++ cypress/e2e/TAT-CSC.cy.js
@@ -100,4 +100,29 @@ describe("TAT Customer Service Center", () => {
 
     cy.get("@checkboxes").last().uncheck().should("not.be.checked");
   });
+
+  it("selects a file from the fixtures folder", () => {
+    cy.get("#file-upload")
+      .selectFile("cypress/fixtures/example.json")
+      .should(($input) => {
+        expect($input[0].files[0].name).to.equal("example.json");
+      });
+  });
+
+  it("selects a file simulating a drag-and-drop", () => {
+    cy.get("#file-upload")
+      .selectFile("cypress/fixtures/example.json", { action: "drag-drop" })
+      .should(($input) => {
+        expect($input[0].files[0].name).to.equal("example.json");
+      });
+  });
+
+  it("selects a file using a fixture to which an alias was given", () => {
+    cy.fixture("example.json").as("sampleFile");
+    cy.get("#file-upload")
+      .selectFile("@sampleFile")
+      .should(($input) => {
+        expect($input[0].files[0].name).to.equal("example.json");
+      });
+  });
 });
